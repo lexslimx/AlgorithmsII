61946fc7a4915defc182cfd90d590c138c99cfd0
alex <alex.km@live.com>
2022-01-06 22:44:25 +0300

    Added insertionsort

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -1,9 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-using AlgorithmsII;
-
-
-int[] nums = new int[]{5,2,3,6,1};
-for (int i = 1; i < nums.Length; i++){
+public class InsertionSort {
+    public void SortColors(int[] nums) {
+        
+        for (int i = 1; i < nums.Length; i++){
     int j = i;
     while (j - 1 >= 0 && nums[j] < nums[j - 1])
     {
@@ -14,4 +12,6 @@ for (int i = 1; i < nums.Length; i++){
         nums[j - 1] = temp;
         j--;
     }
+} 
+    }   
 }
